cb30e87e5ec7a9d6f87b469f544341395e289149
philo-py <okaimono3776@gmail.com>
2024-12-24 13:15:43 +0900

    1.01

--- app.js
+++ app.js
@@ -11,21 +11,31 @@ const message = document.getElementById("message");
 checkBtn.addEventListener("click", () => {
   const guess = Number(guessInput.value) //入力値を数値に変換
   attempts++; //試行回数をプラスする
-})
 
 //入力をチェック
-if (!guess) {
-  message.textContent = "数字を入力してください！"
-  return;
-}
+  if (!guess) {
+    message.textContent = "数字を入力してください！"
+    return;
+  }
 
-//判定
-if (guess < secretNumber) {
-  message.textContent = "もっと大きい数字です！"
-} else if (guess > secretNumber) {
-  message.textContent = "もっと小さな数字です！"
-} else {
-  message.textContent = "おめでとうございます！正解です！ (${attempts}回で正解）"
-  checkBtn.disabled = true; //ボタンの無効化
-  resetBtn.style.display = "inline" //リセットボタンを表示
-}
+  //判定
+  if (guess < secretNumber) {
+    message.textContent = "もっと大きい数字です！"
+  } else if (guess > secretNumber) {
+    message.textContent = "もっと小さな数字です！"
+  } else {
+    message.textContent = `おめでとうございます！正解です！ (${attempts}回で正解）`;
+    checkBtn.disabled = true; //ボタンの無効化
+    resetBtn.style.display = "inline"; //リセットボタンを表示
+  }
+});
+
+//リセットボタンを押したときの処理
+resetBtn.addEventListener ( "click", () => {
+  secretNumber = Math.floor(Math.random() * 100) + 1; //新しい数字を設定
+  attempts = 0; //試行回数をリセット
+  message.textContent = ""; //メッセージをリセット
+  guessInput.value = ""; //入力欄を空にする
+  checkBtn.disabled = false; //ボタンの有効化
+  resetBtn.style.display = "none"; //リセットボタンを隠す
+});
